23b376f393bb9e7183a676216fef4228679d088b
fernandotassi <apropriate3@gmail.com>
2022-12-14 13:40:15 -0300

    Excessão personalizada e programação defensiva

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -1,5 +1,7 @@
 package tabuleiro;
 
+import tabuleiro.TabuleiroExcessao;
+
 public class Tabuleiro
 {
 	private Integer linhas;
@@ -7,25 +9,52 @@ public class Tabuleiro
 	private Peca[][] peca;
 	
 	public Tabuleiro(Integer linhas, Integer colunas)
-	{this.linhas = linhas; this.colunas = colunas; peca = new Peca[linhas][colunas];}
-	
-	public void setLinhas(Integer linhas){this.linhas = linhas;}
-	
-	public void setColunas(Integer colunas){this.colunas = colunas;}
-	
+	{
+		if(linhas < 1 || colunas < 1)
+		{throw new TabuleiroExcessao("Pelo menis uma linha e uma coluna!");}	
+			
+		this.linhas = linhas; this.colunas = colunas; peca = new Peca[linhas][colunas];
+	}
+		
 	public Integer getLinhas(){return  linhas;}
 	
 	public Integer getColunas(){return colunas;}
 	
 	public Peca peca(Integer linha, Integer coluna)
-	{return peca[linha][coluna];}
+	{
+		if(!posicaoExiste(linha, coluna))
+		{throw new TabuleiroExcessao("Posição não existe no tabuleiro!");}	
+		
+		return peca[linha][coluna];
+    }
 	
 	public Peca peca(Posicao posicao)
-	{return peca[posicao.getLinha()][posicao.getColuna()];}
+	{
+		if(!posicaoExiste(posicao))
+		{throw new TabuleiroExcessao("Posição não existe no tabuleiro!");}	
+		
+		return peca[posicao.getLinha()][posicao.getColuna()];}
 	
 	public void colocaPeca(Peca peca, Posicao posicao)
 	{
+		if(temUmaPeca(posicao))
+		{throw new TabuleiroExcessao("Já tem uma peça ai!" + posicao);}
+		
 		this.peca[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+	
+	private boolean posicaoExiste(Integer linha, Integer coluna)
+	{return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;}
+	
+	public boolean posicaoExiste(Posicao posicao)
+	{return posicaoExiste(posicao.getLinha(), posicao.getColuna());}
+	
+	public boolean temUmaPeca(Posicao posicao)
+	{ 
+		if(!posicaoExiste(posicao))
+		{throw new TabuleiroExcessao("Posição não existe no tabuleiro!");}	
+		
+		return peca(posicao) != null;		
 	}
 }
---
+++ src/tabuleiro/TabuleiroExcessao.java
@@ -0,0 +1,8 @@
+package tabuleiro;
+
+public class TabuleiroExcessao extends RuntimeException
+{
+	private static final long serialVersionUID = 1L;
+    
+	public TabuleiroExcessao(String msg){super(msg);}
+}
