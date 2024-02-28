53d16761c8108b2160b3ed74d53bf062db8097f6
fernandotassi <apropriate3@gmail.com>
2024-02-28 18:54:37 -0300

    projeto criado

---
+++ Programa.java
@@ -0,0 +1,20 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Date;
+
+public class Programa
+{
+	public static void main(String[] args)
+	{
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		Date d = new Date();
+		System.out.println(d);
+	
+	
+	
+	
+		scan.close();
+	}
+}
